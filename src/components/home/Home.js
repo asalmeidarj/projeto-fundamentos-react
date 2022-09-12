94dee8b1c26e09dd7f9337b50c294d03d946e3ed
asalmeidarj <asalmeidarj@gmail.com>
2022-09-12 05:49:53 -0300

    atualização da home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import EfeitoMaquina from '../utils/EfeitoMaquina';
+import EfeitoMaquina from '../utils/efeitoMaquina/EfeitoMaquina';
+
+import { useState } from 'react';
 
 import styles from './home.module.css';
 
 const Home = () => {
   
-  const saudacao =  `Este Projeto foi feito para aplição dos conceitos básicos 
-    de React. Espero que goste!`;
+  const [saudacao] =  useState(`Este Projeto foi feito para aplição dos conceitos básicos 
+    de React. Espero que goste!`);
+
   return (
     <section className={styles.main}>
         <div className={styles.container}>
-            <section className={styles.saudacao}>
-                <EfeitoMaquina texto={saudacao}/>
+            <section className={styles.container_saudacao}>
+                <EfeitoMaquina texto={saudacao} className={styles.efeitoMaquina}/>
             </section>
             <section className={styles.ancora}>
                 <a href='/cards'><button className={'button-85 ' + styles.btn_85 }>Acesse os cards!</button></a>
